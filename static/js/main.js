eab89958c263b73e9c50e734b31ed09c358f426b
wlsn0105 <jack13579@naver.com>
2024-05-02 17:56:50 +0900

    Feat: progress 게이지 바 추가

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,13 @@
-console.log("car");
+import "./scroll-timeline.js";
+
+const progress = document.querySelector(".progress");
+
+progress.animate([{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }], {
+    timeline: new ScrollTimeline({
+        scrollOffsets: [
+            // body 전체 높이에 대한 게이지 바
+            { target: document.body, edge: "start", threshold: 1 },
+            { target: document.body, edge: "end", threshold: 1 },
+        ],
+    }),
+});
