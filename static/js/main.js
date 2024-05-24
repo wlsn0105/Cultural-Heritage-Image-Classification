7ac915288b8f3e0a438ec172d6814ca4b329dd2c
wlsn0105 <jack13579@naver.com>
2024-05-24 15:01:32 +0900

    Feat: 상단 이동 버튼 추가

--- static/js/main.js
+++ static/js/main.js
@@ -54,3 +54,10 @@ gallery.animate([{ transform: "translateX(calc(15%))" }, { transform: "translate
         ],
     }),
 });
+
+const topBtn = document.querySelector(".moveTopBtn");
+
+// 버튼 클릭 시 맨 위로 이동
+topBtn.onclick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
